30949c49f01cb2cf45b2df98ae4377b4cc60a571
aromanelli <adrian.romanelli@gmail.com>
2018-06-27 22:22:10 -0700

    Completed coding of favorite movies functionality.

---
+++ app/src/main/java/info/romanelli/udacity/android/popularmovies/FavMoviesObserver.java
@@ -0,0 +1,94 @@
+package info.romanelli.udacity.android.popularmovies;
+
+import android.arch.lifecycle.LiveData;
+import android.arch.lifecycle.Observer;
+import android.support.annotation.Nullable;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+
+import com.google.gson.Gson;
+import com.google.gson.GsonBuilder;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import info.romanelli.udacity.android.popularmovies.database.AppDatabase;
+import info.romanelli.udacity.android.popularmovies.database.MovieEntry;
+import info.romanelli.udacity.android.popularmovies.network.MovieInfo;
+import info.romanelli.udacity.android.popularmovies.util.MoviesInfoFetcher;
+
+public class FavMoviesObserver implements Observer<List<MovieEntry>> {
+
+    final static private String TAG = FavMoviesObserver.class.getSimpleName();
+
+    final private MoviesInfoFetcher.Listener listener;
+    private LiveData<List<MovieEntry>> ld;
+
+    private MoviesInfoFetcher.MoviesInfoType moviesInfoType;
+
+    public FavMoviesObserver( final MoviesInfoFetcher.Listener listener,
+                              final MoviesInfoFetcher.MoviesInfoType moviesInfoType) {
+        Log.d(TAG, "FavMoviesObserver() called with: listener = [" + listener + "], moviesInfoType = [" + moviesInfoType + "]");
+
+        this.listener = listener;
+        this.moviesInfoType = moviesInfoType;
+    }
+
+    @Override
+    public void onChanged(@Nullable final List<MovieEntry> movieEntries) {
+        Log.d(TAG, "onChanged() called with: movieEntries = [" + movieEntries + "]");
+
+        // If user changes favorite from within favorites view ...
+        Log.d(TAG, "onChanged: moviesInfoType: " + moviesInfoType);
+        if (MoviesInfoFetcher.MoviesInfoType.FAVORITES.equals(moviesInfoType)) {
+
+            // Convert received data into MovieInfo objects ...
+            AppExecutors.$().diskIO().execute(new Runnable() {
+                @Override
+                public void run() {
+                    // Deserialize the previously saved MovieInfo(MovieEntry) entries ...
+                    // (Serialization done in DetailActivity, off the favorites button.)
+                    final ArrayList<MovieInfo> list;
+                    if (movieEntries != null && (movieEntries.size() >= 1)) {
+                        final Gson gson = new GsonBuilder().create();
+                        list = new ArrayList<>(movieEntries.size());
+                        for (MovieEntry me : movieEntries) {
+                            list.add(gson.fromJson(me.getJson(), MovieInfo.class));
+                        }
+                    } else {
+                        // No favorites, so set the model for the view to an empty list ...
+                        list = new ArrayList<>(0);
+                    }
+                    AppExecutors.$().mainThread().execute(new Runnable() {
+                        @Override
+                        public void run() {
+                            listener.fetchedMoviesInfo(list);
+                        }
+                    });
+                }
+            });
+
+        }
+    }
+
+    synchronized public void start(AppCompatActivity activity) throws IllegalStateException {
+        if (ld != null) {
+            throw new IllegalStateException("You must call stop() before calling start(Activity)!");
+        }
+        Log.d(TAG, "start() called");
+        ld = AppDatabase.$(activity).movieDao().getAllMovies();
+        ld.observe(activity,this);
+    }
+
+    synchronized public void stop() {
+        Log.d(TAG, "stop() called");
+        ld.removeObserver(this);
+        ld = null;
+    }
+
+    public void setMoviesInfoType(final MoviesInfoFetcher.MoviesInfoType type) {
+        Log.d(TAG, "setMoviesInfoType() called with: type = [" + type + "]");
+        this.moviesInfoType = type;
+    }
+
+}
--- app/src/main/java/info/romanelli/udacity/android/popularmovies/util/MoviesInfoFetcher.java
+++ app/src/main/java/info/romanelli/udacity/android/popularmovies/util/MoviesInfoFetcher.java
@@ -1,23 +1,16 @@
 package info.romanelli.udacity.android.popularmovies.util;
 
-import android.arch.lifecycle.Observer;
 import android.support.annotation.NonNull;
-import android.support.annotation.Nullable;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 
-import com.google.gson.Gson;
-import com.google.gson.GsonBuilder;
 import com.google.gson.annotations.SerializedName;
 
 import java.net.URL;
 import java.util.ArrayList;
-import java.util.List;
 
-import info.romanelli.udacity.android.popularmovies.AppExecutors;
 import info.romanelli.udacity.android.popularmovies.BuildConfig;
-import info.romanelli.udacity.android.popularmovies.database.AppDatabase;
-import info.romanelli.udacity.android.popularmovies.database.MovieEntry;
+import info.romanelli.udacity.android.popularmovies.FavMoviesObserver;
 import info.romanelli.udacity.android.popularmovies.network.MovieInfo;
 import retrofit2.Call;
 import retrofit2.Callback;
@@ -29,6 +22,8 @@ public class MoviesInfoFetcher extends AbstractFetcher {
 
     final static private String TAG = MoviesInfoFetcher.class.getSimpleName();
 
+    static private FavMoviesObserver OBSERVER;
+
     synchronized static public void fetchMoviesInfo( final AppCompatActivity activity,
                                                      final MoviesInfoFetcher.Listener listener,
                                                      final MoviesInfoType moviesInfoType ) {
@@ -36,99 +31,75 @@ public class MoviesInfoFetcher extends AbstractFetcher {
         Log.d(TAG, "fetchMoviesInfo() called with: activity = [" + activity + "], listener = [" +
                 listener + "], moviesInfoType = [" + moviesInfoType + "]");
 
+        if (OBSERVER != null) {
+            // Update the observer for non-Favorites type viewing in progress, so that it
+            // does not do favorites list building/setting for non-favorite views...
+            OBSERVER.setMoviesInfoType(moviesInfoType);
+        }
+
         if (MoviesInfoType.FAVORITES.equals(moviesInfoType)) {
 
             /////////////////////////////////////////
             // Need to use locally stored (in DB) ...
             /////////////////////////////////////////
 
-            AppDatabase.$(activity.getApplicationContext()).movieDao().getAllMovies().observe(
-                    activity,
-                    new Observer<List<MovieEntry>>() {
-                        @Override
-                        public void onChanged(@Nullable final List<MovieEntry> movieEntries) {
-                            Log.d(TAG, "onChanged() called with: movieEntries = [" + movieEntries + "] " + Thread.currentThread().getName());
+            if (OBSERVER != null) {
+                // Not sure if this stop() is needed to prevent memory leaks, or if the
+                // Android Observer class garbage collects well after de-assignment??
+                OBSERVER.stop();
+            }
+            OBSERVER = new FavMoviesObserver(listener, moviesInfoType);
+            OBSERVER.start(activity);
+            return;
 
-                            // Stop listening for changes, now that we've received the data ...
-                            AppDatabase.$(activity.getApplicationContext()).movieDao().getAllMovies().removeObserver(this);
+        }
 
-                            AppExecutors.$().diskIO().execute(new Runnable() {
-                                @Override
-                                public void run() {
-                                    // Deserialize the previously saved MovieInfo(MovieEntry) entries ...
-                                    // (Serialization done in DetailActivity, off the favorites button.)
-                                    final ArrayList<MovieInfo> list;
-                                    if (movieEntries != null && (movieEntries.size() >= 1)) {
-                                        final Gson gson = new GsonBuilder().create();
-                                        list = new ArrayList<>(movieEntries.size());
-                                        for (MovieEntry me : movieEntries) {
-                                            list.add(gson.fromJson(me.getJson(), MovieInfo.class));
-                                        }
-                                    }
-                                    else {
-                                        // No favorites, so set the model for the view to an empty list ...
-                                        list = new ArrayList<>(0);
-                                    }
-                                    AppExecutors.$().mainThread().execute(new Runnable() {
-                                        @Override
-                                        public void run() {
-                                            listener.fetchedMoviesInfo(list);
-                                        }
-                                    });
-                                }
-                            });
-                        }
+        ///////////////////////////////////////
+        // Need to call out to the Internet ...
+        ///////////////////////////////////////
+
+        if (InfoFetcherUtil.isOnline(activity)) {
+            Log.d(TAG, "fetchMoviesInfo(): Online; starting Retrofit process for list type [" +
+                    moviesInfoType + "] ...");
+
+            // Call instance can be exec'd only once!
+            Call<MoviesInfoFetcher.Response> call =
+                    RETROFIT.create(MoviesInfoFetcher.Service.class)
+                            .fetchMoviesInfo(
+                                    moviesInfoType.getURL(),
+                                    // https://medium.com/code-better/hiding-api-keys-from-your-android-repository-b23f5598b906
+                                    // https://stackoverflow.com/a/34021467/435519 (option #2)
+                                    //    https://stackoverflow.com/questions/33134031/is-there-a-safe-way-to-manage-api-keys
+                                    BuildConfig.ApiKey_TheMovieDB
+                            );
+            Log.d(TAG, "fetchMoviesInfo(): enqueue; Uri for list of movies: [" + call.request().url() + "]");
+            call.enqueue(new Callback<MoviesInfoFetcher.Response>() {
+                @Override
+                public void onResponse(
+                        @NonNull final Call<MoviesInfoFetcher.Response> call,
+                        @NonNull final retrofit2.Response<MoviesInfoFetcher.Response> response) {
+                    Log.d(TAG, "onResponse: body: " + response.body());
+                    if (response.body() != null) {
+                        //noinspection ConstantConditions
+                        listener.fetchedMoviesInfo(response.body().getMoviesInfo());
+                    } else {
+                        Log.e(TAG, "onResponse: No response body was returned!");
                     }
-            );
+                }
+
+                @Override
+                public void onFailure(
+                        @NonNull final Call<MoviesInfoFetcher.Response> call,
+                        @NonNull final Throwable t) {
+                    Log.e(TAG, "onFailure: ", t);
+                }
+            });
+
         }
         else {
-
-            ///////////////////////////////////////
-            // Need to call out to the Internet ...
-            ///////////////////////////////////////
-
-            if (InfoFetcherUtil.isOnline(activity)) {
-                Log.d(TAG, "fetchMoviesInfo(): Online; starting Retrofit process for list type [" +
-                        moviesInfoType + "] ...");
-
-                // Call instance can be exec'd only once!
-                Call<MoviesInfoFetcher.Response> call =
-                        RETROFIT.create(MoviesInfoFetcher.Service.class)
-                                .fetchMoviesInfo(
-                                        moviesInfoType.getURL(),
-                                        // https://medium.com/code-better/hiding-api-keys-from-your-android-repository-b23f5598b906
-                                        // https://stackoverflow.com/a/34021467/435519 (option #2)
-                                        //    https://stackoverflow.com/questions/33134031/is-there-a-safe-way-to-manage-api-keys
-                                        BuildConfig.ApiKey_TheMovieDB
-                                );
-                Log.d(TAG, "fetchMoviesInfo(): enqueue; Uri for list of movies: [" + call.request().url() + "]");
-                call.enqueue(new Callback<MoviesInfoFetcher.Response>() {
-                    @Override
-                    public void onResponse(
-                            @NonNull final Call<MoviesInfoFetcher.Response> call,
-                            @NonNull final retrofit2.Response<MoviesInfoFetcher.Response> response) {
-                        Log.d(TAG, "onResponse: body: " + response.body());
-                        if (response.body() != null) {
-                            //noinspection ConstantConditions
-                            listener.fetchedMoviesInfo(response.body().getMoviesInfo());
-                        } else {
-                            Log.e(TAG, "onResponse: No response body was returned!");
-                        }
-                    }
-
-                    @Override
-                    public void onFailure(
-                            @NonNull final Call<MoviesInfoFetcher.Response> call,
-                            @NonNull final Throwable t) {
-                        Log.e(TAG, "onFailure: ", t);
-                    }
-                });
-
-            }
-            else {
-                Log.w(TAG, "fetchMoviesInfo: Not online, so cannot do fetching; aborting!");
-            }
+            Log.w(TAG, "fetchMoviesInfo: Not online, so cannot do fetching; aborting!");
         }
+
     }
 
     public enum MoviesInfoType {
